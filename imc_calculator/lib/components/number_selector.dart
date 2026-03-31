87d84a6e25f7fc74681dd6c3b5536da12e987fe5
pascual214 <adrian.pascualblesa@gmail.com>
2026-03-31 11:43:07 +0200

    Implementación peso y edad

---
+++ imc_calculator/lib/components/number_selector.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_color.dart';
+import 'package:imc_calculator/core/text_styles.dart';
+
+class NumberSelector extends StatefulWidget {
+  final String title;
+  final int value;
+  final Function() onIncrement;
+  final Function() onDecrement;
+
+  const NumberSelector({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onIncrement,
+    required this.onDecrement,
+  });
+
+  @override
+  State<NumberSelector> createState() => _NumberSelectorState();
+}
+
+class _NumberSelectorState extends State<NumberSelector> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColor.backgroundComponent,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Text(widget.title, style: TextStyles.bodyText),
+            Text(widget.value.toString(), style: TextStyles.bodyText),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    widget.onDecrement();
+                  },
+                  shape: CircleBorder(),
+                  backgroundColor: AppColor.primary,
+                  foregroundColor: Colors.white,
+                  child: Icon(Icons.remove),
+                ),
+                SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: () {
+                    widget.onIncrement();
+                  },
+                  shape: CircleBorder(),
+                  backgroundColor: AppColor.primary,
+                  foregroundColor: Colors.white,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- imc_calculator/lib/screens/imc_home_screen.dart
+++ imc_calculator/lib/screens/imc_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
 import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -10,12 +11,55 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 40;
+  int selectedWeight = 70;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GenderSelector(),
         HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "Peso".toUpperCase(),
+                  value: selectedWeight,
+                  onIncrement: () {
+                    setState(() {
+                      selectedWeight++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedWeight--;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "Edad".toUpperCase(),
+                  value: selectedAge,
+                  onIncrement: () {
+                    setState(() {
+                      selectedAge++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedAge--;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
