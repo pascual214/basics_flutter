7519a872b93e9ff7c91750fab68dfc8b54c78e6d
pascual214 <adrian.pascualblesa@gmail.com>
2026-04-01 14:44:19 +0200

    Implementación powerstats

--- superhero_app/lib/data/model/superhero_detail_response.dart
+++ superhero_app/lib/data/model/superhero_detail_response.dart
@@ -2,11 +2,15 @@ class SuperheroDetailResponse {
   final String id;
   final String name;
   final String url;
+  final String realName;
+  final PowerstatsResponse powerstatsResponse;
 
   SuperheroDetailResponse({
     required this.id,
     required this.name,
     required this.url,
+    required this.realName,
+    required this.powerstatsResponse,
   });
 
   factory SuperheroDetailResponse.fromJson(Map<String, dynamic> json) {
@@ -14,6 +18,37 @@ class SuperheroDetailResponse {
       id: json["id"],
       name: json["name"],
       url: json["image"]["url"],
+      realName: json["biography"]["full-name"],
+      powerstatsResponse: PowerstatsResponse.fromJson(json["powerstats"])
+    );
+  }
+}
+
+class PowerstatsResponse {
+  final String intelligence;
+  final String strength;
+  final String speed;
+  final String durability;
+  final String power;
+  final String combat;
+
+  PowerstatsResponse({
+    required this.intelligence,
+    required this.strength,
+    required this.speed,
+    required this.durability,
+    required this.power,
+    required this.combat,
+  });
+
+  factory PowerstatsResponse.fromJson(Map<String, dynamic> json) {
+    return PowerstatsResponse(
+      intelligence: json["intelligence"],
+      strength: json["strength"],
+      speed: json["speed"],
+      durability: json["durability"],
+      power: json["power"],
+      combat: json["combat"],
     );
   }
 }
--- superhero_app/lib/screens/superhero_detail_screen.dart
+++ superhero_app/lib/screens/superhero_detail_screen.dart
@@ -3,13 +3,97 @@ import 'package:superhero_app/data/model/superhero_detail_response.dart';
 
 class SuperheroDetailScreen extends StatelessWidget {
   final SuperheroDetailResponse superhero;
-  
+
   const SuperheroDetailScreen({super.key, required this.superhero});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(superhero.name),
+      appBar: AppBar(title: Text("Superhero ${superhero.name}")),
+      body: Center(
+        child: Column(
+          children: [
+            Text(superhero.realName, style: TextStyle(fontSize: 28)),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: SizedBox(
+                width: double.infinity,
+                height: 190,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          height: double.parse(
+                            superhero.powerstatsResponse.power,
+                          ),
+                          width: 20,
+                          color: Colors.red,
+                        ),
+                        Text("Poder"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          height: double.parse(
+                            superhero.powerstatsResponse.strength,
+                          ),
+                          width: 20,
+                          color: Colors.grey,
+                        ),
+                        Text("Fuerza"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          height: double.parse(
+                            superhero.powerstatsResponse.intelligence,
+                          ),
+                          width: 20,
+                          color: Colors.blue,
+                        ),
+                        Text("Inteligencia"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          height: double.parse(
+                            superhero.powerstatsResponse.durability,
+                          ),
+                          width: 20,
+                          color: Colors.deepOrange,
+                        ),
+                        Text("Durabilidad"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          height: double.parse(
+                            superhero.powerstatsResponse.combat,
+                          ),
+                          width: 20,
+                          color: Colors.black,
+                        ),
+                        Text("Combate"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
